2897fd736b3d87b93a8c4d663167b8ed2a4aae7a
ezraabrari <ezraabrari13@gmail.com>
2024-03-29 19:08:43 +0700

    Membuat Proses quick sort

--- QuickSort_0059/QuickSort_0059.cpp
+++ QuickSort_0059/QuickSort_0059.cpp
@@ -45,4 +45,22 @@ void q_short(int low, int high) {
 	pivot = arr[low];
 	i = low + 1;
 	j = high;
+
+	while (i <= j) {
+		while ((arr[i] <= pivot) && (i <= high)) {
+			i++;
+			cmp_count++;
+		}
+		cmp_count++;
+		while ((arr[j] > pivot) && (j >= low)) {
+			j--;
+			cmp_count++;
+		}
+		cmp_count++;
+		if (i < j) {
+			swap(i, j);
+		}
+
+	}
 }
+
